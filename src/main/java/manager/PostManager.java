b4ebb8bc57fa82bbd080782171e9efb17fadca4a
coursar <Sonia@MacBook-Air-Sofa.local>
2020-07-08 23:50:57 +0300

    Manager added

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,22 @@
+package manager;
+
+import domain.WallPost;
+
+public class PostManager {
+
+    int ownerID;
+    String domain;
+    String query;
+    Boolean ownersOnly;
+    int count;
+    int offsetresults;
+
+    public WallPost[] search() {
+        //ToDo: add logic
+        return null;
+    }
+
+    public void delete(){
+        //ToDo: add logic
+    }
+}
